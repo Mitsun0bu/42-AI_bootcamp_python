1f8f6036836f9a2093e59ed32746bf891e37a5ab
Lucas Lethuillier <llethuil@student.42lyon.fr>
2022-06-23 17:36:42 +0200

    minor changes + docstrings added in ex10

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -2,23 +2,40 @@ import time
 import timeit
 import sys
 
+
 def ft_progress(lst: list) -> None:
+    """In this function :
+    - The starting time of the program is obtained thanks to timeit package.
+    - The length of the progress bar is set.
+    - The for loop iterates in lst from 0 to len(lst).
+    - i_bar is incremented if lst_progress reaches a value that corresponds to
+      a 1% increment (in progress bar referential).
+    - The print_bar function is called.
+    """
     start_time = timeit.default_timer()
     bar_len = 100
     lst_len = len(lst)
     i_bar = 0
     for i_lst in lst:
         yield i_lst
-        if (i_lst / lst_len * bar_len >= i_bar + 1):
+        lst_progress = i_lst / lst_len
+        bar_progress = i_bar / bar_len
+        if (lst_progress >= bar_progress + (1 / bar_len)):
             i_bar += 1
         print_bar(i_bar + 1, i_lst + 1, bar_len, lst_len, start_time)
-    
-def print_bar(i_bar, i_lst, bar_len, lst_len, start_time):
+
+
+def print_bar(i_bar, i_lst, bar_len, lst_len, start_time) -> None:
+    """In this function :
+    - The time elapsed since the program started is obtained.
+    - The progession is calculated (in progress bar referential).
+    - The Estimated Time of Arrival (ETA) is calculated by multiplying :
+        ~ (elapsed_time / progress) : time required for current progress
+        and
+        ~ (1 - progress) : progress still to be made
+    """
     elapsed_time = timeit.default_timer() - start_time
     progress = i_bar / bar_len
-    # ETA means estimated time of arrival
-    # (elapsed_time / progress) : time required for current progress
-    # (1 - progress) : progress still to be made 
     eta = elapsed_time / progress * (1 - progress)
     string = "ETA : {0:.2f}s [{1: >2.0f}%][{2}] {3}/{4} | elapsed time {5:.2f}s"
     bar = string.format(eta,
@@ -27,15 +44,15 @@ def print_bar(i_bar, i_lst, bar_len, lst_len, start_time):
                         i_lst,
                         lst_len,
                         elapsed_time)
-    sys.stdout.flush()
     sys.stdout.write("\r" + bar)
     sys.stdout.flush()
+
 
 if __name__ == "__main__":
     lst = range(1000)
     ret = 0
     for elem in ft_progress(lst):
-        ret += (elem + 3) % 5
-        time.sleep(0.01)
+        ret += 1
+        time.sleep(0.005)
     print()
     print(ret)
